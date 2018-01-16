451b37433f1c54b14835e6ff9d9d26c076c707a2
Olga Hryhorieva <olasya@gmail.com>
2018-01-16 23:39:11 +0200

    test_text-share_with_strange_dictionary.py

--- test_text-share.py
+++ test_text-share.py
@@ -5,31 +5,24 @@ from text_paste import *
 def test_create_paste():
 
     """create link with id"""
-    text = create_paste(text='this is my text', title='title',  name='myname', private=0, lang='Text')
-    print(text)
+    link = create_paste(text='this is my text', title='title',  name='myname', private=0, lang='Text')
+    print "link", link
 
     """paste link and receieve only my id"""
-    my_id = get_paste_id(text)
-    print(my_id)
+    my_id = get_paste_id(link)
+    print "my_id is", my_id
 
 
     """take text from the dict get_paste"""
-    my_get_paste = get_paste(my_id)
-    print (my_get_paste)
+    dictionary_with_data = get_paste(my_id)
+    dictionary_right = get_paste('8aca597c')
+    print "dictionary with data", dictionary_with_data
 
     """take key from the dict"""
-    my_url = my_get_paste([u'url'])
-    print(my_url)
+    my_url = dictionary_with_data['url']
+    print my_url
     
-    assert (my_url == text)
+    assert (my_url == link)
 
-# def test_get_paste_id():
 
-#     """create link with id"""
-#     text = create_paste(text='this is my text', title='title',  name='myname', private=0, lang='Text')
-#     print(text)
-
-#     """paste link and receieve only my id"""
-#     my_id = get_paste_id(text)
-#     print(my_id)
 
