dfa82f600e643ecd73eb5af74f8ecd4e166bf82d
Olga Hryhorieva <olasya@gmail.com>
2018-01-18 00:22:02 +0200

    test_text-share_create_paste+get_paste_id+get_paste_text.py

--- test_text-share.py
+++ test_text-share.py
@@ -1,28 +1,71 @@
 import pytest
-from text_paste import *
+from text_paste_1 import *
 
 
-def test_create_paste():
+# def test_create_paste():
 
-    """create link with id"""
-    link = create_paste(text='this is my text', title='title',  name='myname', private=0, lang='Text')
-    print "link", link
-
-    """paste link and receieve only my id"""
-    my_id = get_paste_id(link)
-    print "my_id is", my_id
+#     """create link with id"""
+#     link = create_paste(text='this is my text', title='some title',  name='myname', private=0, lang='Text')
 
 
-    """take text from the dict get_paste"""
-    dictionary_with_data = get_paste(my_id)
-    dictionary_right = get_paste('8aca597c')
-    print "dictionary with data", dictionary_with_data
-
-    """take key from the dict"""
-    my_url = dictionary_with_data['url']
-    print my_url
+#     """paste link and receieve only my id"""
+#     id_from_the_link = get_paste_id(link)
     
-    assert (my_url == link)
+
+#     """take id from the dict get_paste"""
+#     dictionary_with_data = get_paste(id_from_the_link)
+ 
+
+#     """take key url from the dict"""
+#     url_from_the_dictionary_with_data = dictionary_with_data['url']
+
+    
+#     assert (url_from_the_dictionary_with_data == link)
+
+# def test_get_paste_id():
+
+#     """create link"""
+#     link = create_paste(text='this is my text', title='some title',  name='myname', private=0, lang='Text')
+
+#     """paste link and receieve only my id"""
+#     id_from_the_link = get_paste_id(link)
+    
+#     """take text from the dict get_paste"""
+#     dictionary_with_data = get_paste(id_from_the_link)
+
+#     """take key id from the dict """
+#     id_from_the_dictionary_with_data = dictionary_with_data['pid']
+
+#     assert (id_from_the_link == id_from_the_dictionary_with_data)
+
+# def test_get_paste_raw_text():
+
+#     """create link"""
+#     link = create_paste(text='this is my text', title='some title',  name='myname', private=0, lang='Text')
+
+#     """paste link and receieve only my id"""
+#     id_from_the_link = get_paste_id(link)
+    
+#     """take text from the dict get_paste"""
+#     dictionary_with_data = get_paste(id_from_the_link)
+
+#     """take key raw from the dictinary with data"""
+#     raw_from_the_dictionary_with_data = dictionary_with_data['raw']
+
+#     """take text by id"""
+#     text_from_the_dictionary = get_paste_raw_text(id_from_the_link)
+
+#     assert (raw_from_the_dictionary_with_data == text_from_the_dictionary)
+
+def test_random_paste():
+
+    """create link"""
+    link = create_paste(text='this is my text', title='some title',  name='myname', private=0, lang='Text')
+    
+    """get random paste"""
+    random_paste = get_random_paste()
+
+def 
 
 
 
