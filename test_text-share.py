7bb557de078acd77732bce7c270270c2617aac14
Olga Hryhorieva <33554677+olasya@users.noreply.github.com>
2018-02-11 18:00:31 +0200

    test_text_share.py and tools.py  edited (#5)
    * Add test architecture
    
    * test_text-share.py
    
    * test_text-share_with_strange_dictionary.py
    
    * Add test architecture
    
    * text_paste_ed.py
    
    * test_text-share_create_paste+get_paste_id+get_paste_text.py
    
    * text_paste_delete_return0.py
    
    * test_text-share_add_tests.py
    
    * tools.py
    
    * test_text-share_withot_if.py
    
    * test_get_available_languages.py
    
    * .gitignore_add_old_tests.py
    
    * Update requirements.txt
    
    * Update text_paste.py
    
    delete return 0
    
    * test_text-share_edited.py
    
    * tools.py_add_tuple_with_languages.py
    
    * tools_edited_tuple.py

--- test_text-share.py
+++ test_text-share.py
@@ -3,9 +3,10 @@ from text_paste import *
 from tools import *
 
 
-#Bug #1 Expire is -1 instead of positive Value
-def test_create_paste_with_expire_negative():
 
+@pytest.mark.skip(reason = "#Bug #1 Expire is -1 instead of positive Value")
+def test_create_paste_with_expire_negative():
+    """Test create paste with expire field equal -1 """
     link = create_paste(text='text', title='',  name='Olga', private=0, lang='Text', expire='-1' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
@@ -15,6 +16,7 @@ def test_create_paste_with_expire_negative():
 
 def test_create_paste_with_expire_zer0():
 
+    """Test create paste with expire field equal 0 """
     link = create_paste(text='text', title='',  name='Olga', private=0, lang='Text', expire='0' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
@@ -22,39 +24,46 @@ def test_create_paste_with_expire_zer0():
 
     assert (url_from_the_dictionary_with_data == link)
 
-#bug #2 Expire is string instead of Int Value (time)
+
+@pytest.mark.skip(reason = "#Bug #2 Expire is string instead of Int Value (time)")
 def test_create_paste_with_expire_string():
+    """Test create paste with expire fiels equal STRING"""
 
-    link = create_paste(text='text', title='',  name='Olga', private=0, lang='Text', expire='0' )
+    link = create_paste(text='text', title='',  name='Olga', private=0, lang='Text', expire='abc' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
     url_from_the_dictionary_with_data = dictionary_with_data['url']
 
     assert (url_from_the_dictionary_with_data == link)
 
-#Bug #3 Value Private 3 instead of 1 or 0 
+
+@pytest.mark.skip(reason = "#Bug #3 Value Private 3 instead of 1 or 0")
 def test_create_paste_with_private_number_3():
+    """Test create paste with Private value equal 3 """
 
-    link = create_paste(text='text', title='',  name='Olga', private=3, lang='Text', expire='2' )
+    link = create_paste(text='text', title='',  name='Olga', private=3, lang='Text', expire='1' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
     url_from_the_dictionary_with_data = dictionary_with_data['url']
 
     assert (url_from_the_dictionary_with_data == link)
 
-#Bug #4 Value Private 3 instead of 1 or 0 
+
+@pytest.mark.skip(reason = "#Bug #4 Value Private equal STRING instead of 1 or 0") 
 def test_create_paste_with_private_number_string():
+    """Test create paste with Private value equal 'abc' """
 
-    link = create_paste(text='text', title='',  name='Olga', private='1', lang='Text', expire='1' )
+    link = create_paste(text='text', title='',  name='Olga', private='abc', lang='Text', expire='1' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
     url_from_the_dictionary_with_data = dictionary_with_data['url']
 
     assert (url_from_the_dictionary_with_data == link)
 
-#Bug #5 Value Private 3 instead of 1 or 0 
-def test_create_paste_with_private_negative():
 
+@pytest.mark.skip(reason = "#Bug #5 Value Private -1 instead of 1 or 0") 
+def test_create_paste_with_private_negative():
+    """Test create paste with Private value equal -1 """
     link = create_paste(text='text', title='',  name='Olga', private=-1, lang='Text', expire='1' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
@@ -62,9 +71,10 @@ def test_create_paste_with_private_negative():
 
     assert (url_from_the_dictionary_with_data == link)
 
-# Bug #6 Empty field language
-def test_create_paste_with_expire_string():
 
+@pytest.mark.skip(reason = "# Bug #6 Empty field language")
+def test_create_paste_with_empty_field_lang():
+    """Test create paste with empty field lang """
     link = create_paste(text='text', title='',  name='Olga', private=0, lang=' ', expire='1' )
     id_from_the_link = get_paste_id(link)
     dictionary_with_data = get_paste(id_from_the_link)
@@ -73,7 +83,8 @@ def test_create_paste_with_expire_string():
     assert (url_from_the_dictionary_with_data == link)
 
 def test_create_paste():
-    """ 1.create link with id
+    """ Test create paste
+        1.create link
         2.paste link and receieve only my id
         3.take id from the dict get_paste
         4.take key url from the dict
@@ -86,7 +97,8 @@ def test_create_paste():
     assert (url_from_the_dictionary_with_data == link)  
 
 def test_get_paste_raw_text():
-    """ 1.create link
+    """ Test Get raw text paste
+        1.create link
         2.paste link and receieve only my id
         3.take text from the dict get_paste
         4.take key raw from the dictinary with data
@@ -100,7 +112,8 @@ def test_get_paste_raw_text():
     assert (raw_from_the_dictionary_with_data == text_from_the_dictionary)
 
 def test_get_paste_id():
-    """ 1.create link
+    """ Test get ID from the paste text
+        1.create link
         2.paste link and receieve only my id
         3.take text from the dict get_paste
         4.take key id from the dict """
@@ -112,15 +125,21 @@ def test_get_paste_id():
     assert (id_from_the_link == id_from_the_dictionary_with_data)
 
 def test_get_list_available_languages_len():
-    """len of ditctionary with the languages """
-    dictionary_with_languages = get_list_available_languages()
-
+    """Test len of dictionary with the languages """
+   dictionary_with_languages = get_list_available_languages()
+  
     assert(len(dictionary_with_languages) > 30)
 
 def test_get_list_available_languages():
+    """ Test get list available languages 
+        Returns:
+            True: all languages are present ,like in documentation"""
      
     assert(tools_get_list_available_languages())
 
 def test_get_random_paste():
+    """ Test get random paste
+        Returns:
+            True: required fields('lang', 'title', 'name', 'url') are present in the paste"""
  
     assert(tools_get_random_paste())
